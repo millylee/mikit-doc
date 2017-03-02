6f8285417932c6051b5edffd8d7851862ec1aff4
MillyLee <millylee@qq.com>
2017-03-03 01:05:11 +0800

    add offline feature

---
+++ serviceworker.js
@@ -0,0 +1,25 @@
+var cacheName = 'mikit-doc';
+var cacheList = [
+    self.location.hostname,
+    'unpkg.com'
+];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(cacheList);
+        })
+    );
+});
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
